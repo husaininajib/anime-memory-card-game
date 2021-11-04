befed8470f437923144a1423adbfbd298cf0b1cc
Husaini Najib <husaininajib98@gmail.com>
2021-11-05 00:35:15 +0800

    working

--- script.js
+++ script.js
@@ -1,8 +1,75 @@
-const cards = document.querySelectorAll(".card-item")
+const cards = document.querySelectorAll(".card-item");
+let hasFlippedCard = false;
+let firstCard, secondCard;
+let lockCard = false;
 
 function flipCard() {
-    cards.classList.toggle("flip")
+    if (lockCard) return;
+    if (this === firstCard) return;
+
+    this.classList.add("flip");
+
+    if (!hasFlippedCard) {
+    hasFlippedCard = true;
+    firstCard = this;
+    return
+    } 
+    secondCard = this;
+    checkForMatch()
+    
+    // else {
+    // hasFlippedCard = false;
+    // secondCard = this;
+    // checkForMatch()
+    // }
 }
+
+function disableCards() {
+    firstCard.removeEventListener("click", flipCard);
+    secondCard.removeEventListener("click", flipCard);
+    resetCard()
+}
+
+function unflipCard() {
+    lockCard = true;
+
+    setTimeout( () => {
+    firstCard.classList.remove("flip");
+    secondCard.classList.remove("flip");
+    resetCard()
+    }, 1200)
+}
+
+function resetCard() {
+    [hasFlippedCard, lockCard] = [false, false];
+    [firstCard, secondCard] = [null, null];
+}
+
+function checkForMatch() {
+    // cara cepat
+
+    let isMatch = firstCard.dataset.framework === secondCard.dataset.framework;
+    isMatch ? disableCards() : unflipCard()
+
+    // cara lambat
+
+    // if (firstCard.dataset.framework === secondCard.dataset.framework) {
+    // disableMatch()
+    // } else {
+    // unflipCard()
+    // }
+}
+
+(function shuffle() {
+    cards.forEach(card => {
+        let randomPosition = Math.floor(Math.random() * 12);
+        card.style.order = randomPosition
+    })
+})();
+
 cards.forEach(card => {
     card.addEventListener("click", flipCard)
 })
+
+
+
